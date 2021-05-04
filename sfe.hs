34e8b13d16cd963befa24446b63e3eaca493b5e2
Stefan Radziuk <stefan.radziuk19@imperial.ac.uk>
2021-05-04 22:07:34 +0100

    init

---
+++ sfe.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE NumericUnderscores #-}
+
+----
+
+yearsTillWrittenOff = 30
+
+----
+
+rpi = 0.026
+
+additionalInterest = 0.03
+
+repaymentRate = 0.09
+
+salaryIncreaseRate = 0.05
+
+----
+
+repaymentThreshold = 27_295
+
+higherInterestThreshold = 49_130
+
+initialDebt = 42_478
+
+---
+
+type Debt = Float
+
+type Earnings = Float
+
+type Payment = Float
+
+type YearData = (Debt, Earnings, Payment)
+
+initialYearData :: Earnings -> YearData
+initialYearData initialSalary = (initialDebt, initialSalary, debtRepayment initialDebt initialSalary)
+
+debtRepayment :: Debt -> Earnings -> Payment
+debtRepayment debt earnings = min debt (max 0 repayment)
+  where
+    repayment = (earnings - repaymentThreshold) * repaymentRate
+
+nextYearData :: YearData -> YearData
+nextYearData (debt, earnings, payment) = (debt', earnings', debtRepayment debt' earnings')
+  where
+    debt' = (debt - payment) * (1 + rpi + additionalInterest)
+    earnings' = earnings * (1 + salaryIncreaseRate)
+
+run :: Earnings -> [YearData]
+run initialSalary = take yearsTillWrittenOff (iterate nextYearData (initialYearData initialSalary))
+
+totalPaid :: Earnings -> Payment
+totalPaid initialSalary = sum (map (\(_, _, p) -> p) (run initialSalary))
